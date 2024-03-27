2d1da45eed72b009720c6f117d86c9448494c7a6
AN <nalawadeamit87@gmail.com>
2024-03-26 21:59:48 -0400

    feat: create initial repo for flickr image search

---
+++ FlickrImageSearch/FlickrImageSearch/FlickrImageSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrImageSearchApp.swift
+//  FlickrImageSearch
+//
+//  Created by Amit Nalawade on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrImageSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
